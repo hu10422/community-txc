4cb1288b666371b7096f6240ad0880a44af520cb
howitt <1662143230@qq.com>
2020-12-21 13:24:53 +0800

    主动获取反馈信息

---
+++ src/main/java/top/tanpu/community/service/TxcService.java
@@ -0,0 +1,16 @@
+package top.tanpu.community.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TxcService {
+
+
+    public List getFeedback() {
+
+        return null;
+    }
+
+}
